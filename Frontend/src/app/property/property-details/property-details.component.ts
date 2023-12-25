fc6cb464ad0f317991961321db6befcc77d7c82d
Abhay Rajde <abrajde@iu.edu>
2023-12-25 00:37:46 -0500

    routing to the same component, w/o refreshing

--- Frontend/src/app/property/property-details/property-details.component.ts
+++ Frontend/src/app/property/property-details/property-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
     selector: 'app-property-details',
@@ -8,12 +8,21 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class PropertyDetailsComponent implements OnInit {
     public propertyId!: number;
-    constructor(private route: ActivatedRoute) {
+    constructor(private route: ActivatedRoute, private router: Router) {
 
     }
 
     ngOnInit(): void {
-        this.propertyId = this.route.snapshot.params['id'];
-    }
+        this.propertyId = Number(this.route.snapshot.params['id']);
 
+        this.route.params.subscribe(
+            (params) => {
+                this.propertyId = Number(params['id'])
+            }
+        )
+    }
+    onSelectNext() {
+        this.propertyId += 1;
+        this.router.navigate(["property-details", this.propertyId])
+    }
 }
