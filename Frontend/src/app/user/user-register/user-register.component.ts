1cc6bc9b064b6f05657b195801968705a6a0b9a8
Abhay Rajde <abrajde@iu.edu>
2024-01-07 04:41:05 -0500

    custom validators in rf, global css

--- Frontend/src/app/user/user-register/user-register.component.ts
+++ Frontend/src/app/user/user-register/user-register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-user-register',
@@ -12,9 +12,42 @@ export class UserRegisterComponent implements OnInit {
 
     ngOnInit(): void {
         this.registrationForm = new FormGroup({
-            userName: new FormControl()
-        });
+            userName: new FormControl("Test",Validators.required),
+            email: new FormControl("", [Validators.required, Validators.email]),
+            password: new FormControl("",[Validators.required, Validators.minLength(8)]),
+            confirmPassword: new FormControl("",[Validators.required]),
+            mobile: new FormControl("",[Validators.required, Validators.maxLength(10)]),
+        }, { validators: this.passwordMatchingValidator});
     }
+
+    passwordMatchingValidator(control: AbstractControl): ValidationErrors | null {
+        return (control.get('password')?.value === control.get('confirmPassword')?.value) ? null : 
+        {notMatched: true};
+    }
+
+    // Getter method for all form controls
+    //----------------------------------------------------------------------------
+    get userName() {
+        return this.registrationForm.get('userName') as FormControl;
+    }
+
+    get email() {
+        return this.registrationForm.get('email') as FormControl;
+    }
+
+    get password() {
+        return this.registrationForm.get('password') as FormControl;
+    }
+
+    get confirmPassword() {
+        return this.registrationForm.get('confirmPassword') as FormControl;
+    }
+
+    get mobile() {
+        return this.registrationForm.get('mobile') as FormControl;
+    }
+    // -----------------------------------------------------------------------------
+    
     onSubmit() {
         console.log(this.registrationForm);
         
